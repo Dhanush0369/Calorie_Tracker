be3899fb92e7360aa55a0f8aeb25a136889b0be4
Dhanush0369 <dhanush789245@gmail.com>
2024-02-08 10:40:25 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+
+import Entries from './components/entries.js'
+
+function App() {
+  return (
+    <div>
+      <Entries />
+    </div>
+  );
+}
+
+export default App;
